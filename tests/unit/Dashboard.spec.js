bb1cdf6328b9981ab8ced9d3c1851a721ed5b173
DeepikaPadam <manideepika.padam@gmail.com>
2022-06-27 20:50:58 +0530

    Update Dashboard.spec.js

--- tests/unit/Dashboard.spec.js
+++ tests/unit/Dashboard.spec.js
@@ -1,19 +1,44 @@
-import { mount, shallowMount } from '@vue/test-utils'
+import { mount, RouterLinkStub } from '@vue/test-utils'
 import DashboardView from '@/components/Dashboard.vue'
 
 describe('Mounted App', () => {
-  let wrapper
+    
+  let wrapper;
+  const getData = jest.fn()
   beforeEach(() => {
     wrapper = mount(DashboardView, {
       data() {
         return {
-            showList: '',
+            showList: [],
         }
       },
+      global: {
+        stubs: {
+            'router-link': RouterLinkStub,
+        }
+    },
     })
   })
   test('DashboardView component exists', () => {
     expect(wrapper.exists()).toBe(true)
   })
+  
 
+    global.fetch = jest.fn(() => Promise.resolve({
+        json: () => Promise.resolve(getData)
+    }));
+
+    describe('fetch data', () => {
+        let data;
+        beforeEach(async () => {
+            data = await getData();
+        });
+        it('created() should get called when component mounts.', () => {
+            let spy;
+            spy = jest.spyOn(DashboardView, 'created');
+            wrapper = mount(DashboardView);
+            expect(spy).toHaveBeenCalled();
+            spy.mockReset();
+        });
+    });
  })
